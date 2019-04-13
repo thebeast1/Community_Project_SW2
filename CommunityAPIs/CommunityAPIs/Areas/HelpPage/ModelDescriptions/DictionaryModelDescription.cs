7dbc502cd07792dbf2ff457db953e15316feaf60
unknown <tsniper764@gmail.com>
2019-04-13 13:07:00 +0200

    empty project

---
+++ CommunityAPIs/CommunityAPIs/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CommunityAPIs.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CommunityAPIs/CommunityAPIs/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CommunityAPIs.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
